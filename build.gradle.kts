5230f51a61fdf61cea8f166be3de594c172c81c4
Frederik Bertling <Frederik.Bertling@gmail.com>
2018-10-23 20:19:35 +0200

    Added different source sets for different versions Updated kotlin to 1.2.71 Updated gradle jlink plugin to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.beryx.jlink.data.*
 
 plugins {
-    kotlin("jvm") version "1.2.70"
+    kotlin("jvm") version "1.2.71"
     application
     id("com.zyxist.chainsaw") version "0.3.1"
-    id("org.beryx.jlink") version "2.0.1"
+    id("org.beryx.jlink") version "2.1.0"
 }
 
-val currentOS = org.gradle.internal.os.OperatingSystem.current()
+val currentOS = org.gradle.internal.os.OperatingSystem.current()!!
 val platform = when {
-        currentOS.isWindows() -> "win"
-        currentOS.isLinux() -> "linux"
-        currentOS.isMacOsX() -> "mac"
+        currentOS.isWindows -> "win"
+        currentOS.isLinux -> "linux"
+        currentOS.isMacOsX -> "mac"
         else -> throw GradleException("Unsupported operating system: $currentOS")
 }
 val javaFxVersion = 11
@@ -30,21 +30,39 @@ repositories {
     mavenCentral()
 }
 
+//different source sets for different flavors
+sourceSets {
+    create("sandbox").java.srcDirs("src/sandbox/kotlin", "src/sandbox/java")
+    create("production").java.srcDirs("src/production/kotlin", "src/production/java")
+    when (properties["facet"]) {
+        "sandbox" -> {
+            getByName("main").java.srcDirs("src/sandbox/kotlin", "src/sandbox/java")
+        }
+        "production" -> {
+            getByName("main").java.srcDirs("src/production/kotlin", "src/production/java")
+        }
+        else -> {
+            getByName("main").java.srcDirs("src/sandbox/kotlin", "src/sandbox/java")
+        }
+    }
+}
+
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.2.70")
-    implementation("org.openjfx:javafx-base:${javaFxVersion}:${platform}")
-    implementation("org.openjfx:javafx-controls:${javaFxVersion}:${platform}") {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.2.71")
+    implementation("org.openjfx:javafx-base:$javaFxVersion:$platform")
+    implementation("org.openjfx:javafx-controls:$javaFxVersion:$platform") {
         exclude(module = "javafx-graphics")
     }
-    implementation("org.openjfx:javafx-fxml:${javaFxVersion}:${platform}") {
+    implementation("org.openjfx:javafx-fxml:$javaFxVersion:$platform") {
         exclude(module = "javafx-controls")
     }
-    implementation("org.openjfx:javafx-graphics:${javaFxVersion}:${platform}") {
+    implementation("org.openjfx:javafx-graphics:$javaFxVersion:$platform") {
         exclude(module = "javafx-base")
     }
 }
 
 jlink{
+    javaHome.set(System.getProperty("java.home"))
     launcher (delegateClosureOf<LauncherData> {
         name = "hello"
     })
---
+++ src/sandbox/java/module-info.java
@@ -0,0 +1,8 @@
+module test.kotlin.sandbox {
+    requires kotlin.stdlib;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    exports org.beryx.jlink.test.kotlin;
+}
